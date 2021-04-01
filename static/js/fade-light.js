7248cc963c303d85800f2096c0e86dc3b12b5bb3
banalna <judas.priest999@gmail.com>
2021-04-02 02:22:29 +0300

    add scripts, configs

--- static/js/fade-light.js
+++ static/js/fade-light.js
@@ -1,19 +1,13 @@
+
 // fade bg
 function fade() {
-
     jQuery("#shadow").toggle();
-    console.log('qwe2')
-
 }
 
 function fadeOut() {
     if (!jQuery("#shadow").is(":hidden") && !$(event.target).hasClass('nav-link')) {
         jQuery("#shadow").toggle();
-        console.log('qwe1')
     }
-
-    console.log($(event.target).hasClass('dropdown'))
-    console.log($(event.target))
 }
 
 document.body.addEventListener("click", fadeOut)

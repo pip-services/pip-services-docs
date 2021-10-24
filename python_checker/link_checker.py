b5858e5dfd4cb17370f8f16388e01b9dd2e6d7fd
banalna <judas.priest999@gmail.com>
2021-10-24 18:45:45 +0300

    link checker fix commetn parsing

--- python_checker/link_checker.py
+++ python_checker/link_checker.py
@@ -10,16 +10,24 @@ if __name__ == '__main__':
 
     command = 'cd linkchecker && python linkchecker https://pip-services.github.io/pip-services-docs/ ' \
               '--check-extern --threads 15 --ignore-url \/__ --file-output "csv/utf-8/url_log.txt"'
-        
+
     os.system(command)
 
+    # remove comments
+    with open("linkchecker/url_log.txt", 'r+') as file:
+        data = file.readlines()
+        data = data[3:]
+        file.seek(0)
+        file.writelines(data)
+        file.truncate()
+
     # parse log results
-    csvLogs = pd.read_csv("linkchecker/url_log.txt", sep=";", comment="#")
+    csvLogs = pd.read_csv("linkchecker/url_log.txt", sep=";")
 
     if len(csvLogs) != 0:
         raise_err = False
         for index in range(len(csvLogs)):
-            if csvLogs['result'][index] == '429 too many requests':
+            if '404 Not Found' not in csvLogs['result'][index]:
                 continue
 
             url_name = str(csvLogs['urlname'][index]) + '\n'
@@ -27,7 +35,7 @@ if __name__ == '__main__':
             parent_url = str(csvLogs['parentname'][index]) + '\n'
             real_url = str(csvLogs['url'][index]) + '\n'
             result = str(csvLogs['result'][index]) + '\n'
-            
+
             sys.stderr.write(f"URL: {url_name} Name: {name} "
                              f"Parent URL: {parent_url} Real URL: {real_url} Result: {result} \n")
             raise_err = True
